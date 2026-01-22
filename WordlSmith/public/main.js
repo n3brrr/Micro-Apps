5c8268ac9e631c021b66b0ce59326e6e85ec5605
Ruben <neburtl837@gmail.com>
2026-01-22 21:43:12 +0100

    Making async GET request

--- WordlSmith/public/main.js
+++ WordlSmith/public/main.js
@@ -1,37 +1,39 @@
 // Information to reach API
-const url = 'https://api.datamuse.com/words?sl=';
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jja=';
 
-// Selects page elements
+// Selecting page elements
 const inputField = document.querySelector('#input');
 const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
 // Asynchronous function
-const getSuggestions = () => {
+// Code goes here
+const getSuggestions = async () => {
   const wordQuery = inputField.value;
-  const endpoint = `${url}${wordQuery}`;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
   
-  fetch(endpoint, {cache: 'no-cache'}).then(response => {
-    if (response.ok) {
-      return response.json();
+  try {
+    const response = await fetch(endpoint, {cache: 'no-cache'})
+    if(response.ok){
+    const jsonResponse = await response.json();
+    renderResponse(jsonResponse)
     }
-    throw new Error('Request failed!');
-  }, networkError => {
-    console.log(networkError.message)
-  }).then(jsonResponse => {
-    // renderRawResponse(jsonResponse);
-    renderResponse(jsonResponse);
-  })
 
+  } catch (error) {
+    console.log(error)
+  }
+  
 }
 
-// Clears previous results and display results to webpage
+
+// Clear previous results and display results to webpage
 const displaySuggestions = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
-    responseField.removeChild(responseField.firstChild);
+    responseField.removeChild(responseField.firstChild)
   }
   getSuggestions();
-};
+}
 
 submit.addEventListener('click', displaySuggestions);
