9864a0a8379b41b5fa1d9f8cb69715a3cb62944c
Ruben <neburtl837@gmail.com>
2026-01-26 22:47:47 +0100

    Add Film Finder with API

--- ShortenURL/public/main.js
+++ ShortenURL/public/main.js
@@ -1,5 +1,5 @@
-// Information to reach API
-const apiKey = '<Your API Key>';
+// information to reach API
+const apiKey = '';
 const url = 'https://api.rebrandly.com/v1/links';
 
 // Some page elements
@@ -8,27 +8,28 @@ const shortenButton = document.querySelector('#shorten');
 const responseField = document.querySelector('#responseField');
 
 // Asynchronous functions
-const shortenUrl = () => {
-  const urlToShorten = inputField.value;
+const shortenUrl = async () => {
+	const urlToShorten = inputField.value;
   const data = JSON.stringify({destination: urlToShorten});
-  
-	fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      'apikey': apiKey
-    },
-    body: data
-  }).then(response => {
-    if (response.ok) {
-     return response.json();
-    } 
-  throw new Error('Request failed!');
-  }, networkError => {
-  console.log(networkError.message);
-  }).then(jsonResponse => {
-    renderResponse(jsonResponse)
-  })
+  try {
+    const response = await fetch(
+      // add code here
+      url, {
+        method: 'POST',
+        body: data,
+        headers: {
+          'Content-type': 'application/json',
+          'apikey': apiKey
+        }
+      }
+    );
+		if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // Clear page and call Asynchronous functions
